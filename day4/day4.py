1edcfa861d37052a91d3387f00b801f0c1167b6d
Henrik Gabrielsson <henrikgabrielsson@granular.ag>
2022-12-05 07:34:58 -0600

    readability

--- day4/day4.py
+++ day4/day4.py
@@ -9,21 +9,25 @@ def parse_assignments(line: str) -> typ.Tuple[typ.Tuple[int, int], typ.Tuple[int
     return ((int(elf_1[0]), int(elf_1[1])), (int(elf_2[0]), int(elf_2[1])))
 
 
-fully_contained = 0
-overlap = 0
+def fully_contained(elf_1: typ.Tuple[int, int], elf_2: typ.Tuple[int, int]) -> bool:
+    return elf_1[0] >= elf_2[0] and elf_1[1] <= elf_2[1]
+
+
+def overlap(elf_1: typ.Tuple[int, int], elf_2: typ.Tuple[int, int]) -> bool:
+    return elf_1[0] <= elf_2[1] and elf_1[1] >= elf_2[0]
+
+
+fully_contained_count = 0
+overlap_count = 0
 # with open("sample_input.txt") as input_file:
 with open("input.txt") as input_file:
     for line in input_file.readlines():
         elf_1, elf_2 = parse_assignments(line)
-        if (elf_1[0] >= elf_2[0] and elf_1[1] <= elf_2[1]) or (
-            elf_2[0] >= elf_1[0] and elf_2[1] <= elf_1[1]
-        ):
-            fully_contained = fully_contained + 1
-        
-        if (elf_1[0] <= elf_2[1] and elf_1[1] >= elf_2[0]) or (
-            elf_2[0] <= elf_1[1] and elf_2[1] >= elf_1[0]
-        ):
-            overlap = overlap + 1
+        if fully_contained(elf_1, elf_2) or fully_contained(elf_2, elf_1):
+            fully_contained_count = fully_contained_count + 1
 
-print(fully_contained)
-print(overlap)
+        if overlap(elf_1, elf_2) or overlap(elf_2, elf_1):
+            overlap_count = overlap_count + 1
+
+print(fully_contained_count)
+print(overlap_count)
